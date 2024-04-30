4484065e0dbd68f394b075f485f3bc9213c704f4
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-04-30 14:34:04 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -47,7 +47,7 @@ public class UserResource {
 	
 	@DeleteMapping(value = "/{id}") 
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
-		service.Delete(id);
+		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
